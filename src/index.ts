c107e3a896ada1ca93149ead0da7bb84a55c34a3
danst3in <50937807+danst3in@users.noreply.github.com>
2021-03-02 14:39:14 -0800

    mikroORM success posting to db

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,20 @@ import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import mikroConfig from "./mikro-orm.config";
 const main = async () => {
+  // connect to db
   const orm = await MikroORM.init(mikroConfig);
-
+  // run migrations here in code instead of cli
+  await orm.getMigrator().up();
+  // run sql commands
   //  create instance of post
   const post = orm.em.create(Post, { title: "my first post" });
   // insert post into the database
   await orm.em.persistAndFlush(post);
   // await orm.em.nativeInsert(Post, { title: "my first post 2" });
+
+  // find all posts
+  // const posts = await orm.em.find(Post, {});
+  // console.log(posts);
 };
 
 main().catch((err) => console.error(err));
