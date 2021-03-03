26493d36b01ec68693a4af6ee4bab3d97bd90995
danst3in <50937807+danst3in@users.noreply.github.com>
2021-03-02 19:29:44 -0800

    add createPost mutation

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,6 +1,6 @@
 import { Post } from "../entities/Post";
 import { MyContext } from "../types";
-import { Arg, Ctx, Int, Query, Resolver } from "type-graphql";
+import { Arg, Ctx, Int, Mutation, Query, Resolver } from "type-graphql";
 
 @Resolver()
 export class PostResolver {
@@ -10,10 +10,19 @@ export class PostResolver {
   }
 
   @Query(() => Post, { nullable: true })
-  post(
-    @Arg("id", () => Int) id: number,
-    @Ctx() { em }: MyContext
-  ): Promise<Post | null> {
+  post(@Arg("id") id: number, @Ctx() { em }: MyContext): Promise<Post | null> {
     return em.findOne(Post, { id });
+  }
+
+  @Mutation(() => Post)
+  async createPost(
+    @Arg("title") title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Post> {
+    //  create instance of post
+    const post = em.create(Post, { title });
+    // insert post into the database
+    await em.persistAndFlush(post);
+    return post;
   }
 }
